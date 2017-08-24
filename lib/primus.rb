793eb84198821bcc18d57059d13ec55c07e7e1e1
Thomas William Page <tom.w.page@gmail.com>
2017-08-24 21:22:24 +0200

    second

--- lib/primus.rb
+++ lib/primus.rb
@@ -5,21 +5,11 @@ require 'primus/version'
 module Primus
   class Reorder
     def call(hash)
-      hash_empty?(hash)
-      take_odd_keys(hash)
-    end
-
-    private
-
-    def hash_empty?(hash)
       raise 'This hash is empty' if hash.empty?
-    end
-
-    def take_odd_keys(hash)
       array = hash.each_with_index.map { |(key), index| key.upcase if index.odd? }.compact
       array.sort.reverse.join('')
     end
-    # add rubo cop, frozen string, revise sort each with index, each_slice,
-    # slice_each, call this without caiing .new
   end
+  # add rubo cop, frozen string, revise sort each with index, each_slice,
+  # slice_each, call this without caiing .new
 end
