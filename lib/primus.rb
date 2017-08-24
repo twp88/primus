b48502cb5b505eacbbb16ffb82509a3193a49ddf
Thomas William Page <tom.w.page@gmail.com>
2017-08-24 21:20:27 +0200

    first

--- lib/primus.rb
+++ lib/primus.rb
@@ -1,25 +1,25 @@
-require "primus/version"
+# frozen_string_literal: true
+
+require 'primus/version'
 
 module Primus
   class Reorder
     def call(hash)
-      is_hash_empty?(hash)
-      sort_array(take_odd_keys(hash))
+      hash_empty?(hash)
+      take_odd_keys(hash)
     end
 
     private
 
-    def is_hash_empty?(hash)
-     raise RuntimeError.new("This hash is empty") if hash.empty?
+    def hash_empty?(hash)
+      raise 'This hash is empty' if hash.empty?
     end
 
     def take_odd_keys(hash)
-      hash.each_with_index.map { |(key,value), index| key.upcase if index.odd? }.compact
+      array = hash.each_with_index.map { |(key), index| key.upcase if index.odd? }.compact
+      array.sort.reverse.join('')
     end
-
-    def sort_array(array)
-      array.sort.reverse.join("")
-    end
-#add rubo cop, frozen string, revise sort each with index, each_slice, slice_each, call this without caiing .new
+    # add rubo cop, frozen string, revise sort each with index, each_slice,
+    # slice_each, call this without caiing .new
   end
 end
--- spec/primus_spec.rb
+++ spec/primus_spec.rb
@@ -1,41 +1,42 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe Primus do
   let(:subject) { Primus::Reorder.new }
 
-  context "when passing primus a long hash" do
+  context 'when passing primus a long hash' do
     let(:long_hash) do
       {
-        "a" => "This",
-        "b" => "is",
-        "c" => "a",
-        "d" => "longish",
-        "e" => "hash",
-        "f" => "for",
-        "g" => "testing",
-        "h" => "purposes",
-        "i" => "!",
+        'a' => 'This',
+        'b' => 'is',
+        'c' => 'a',
+        'd' => 'longish',
+        'e' => 'hash',
+        'f' => 'for',
+        'g' => 'testing',
+        'h' => 'purposes',
+        'i' => '!'
       }
     end
 
-    it "returns a capitlized string" do
-      expect(subject.call(long_hash)).to eq "HFDB"
+    it 'returns a capitlized string' do
+      expect(subject.call(long_hash)).to eq 'HFDB'
     end
   end
 
-  context "when passing primus a short hash" do
-    let(:short_hash) { { "a" => "Indeed", "b" => "a", "c" => "short_hash" } }
+  context 'when passing primus a short hash' do
+    let(:short_hash) { { 'a' => 'Indeed', 'b' => 'a', 'c' => 'short_hash' } }
 
-    it "returns one capitlized letter" do
-      expect(subject.call(short_hash)).to eq "B"
+    it 'returns one capitlized letter' do
+      expect(subject.call(short_hash)).to eq 'B'
     end
   end
 
-  context "when passing primus an empty hash" do
+  context 'when passing primus an empty hash' do
     let(:empty_hash) { {} }
 
-    it "returns an error message" do
-      expect { subject.call(empty_hash) }.to raise_error(RuntimeError, "This hash is empty")
+    it 'returns an error message' do
+      expect { subject.call(empty_hash) }
+        .to raise_error(RuntimeError, 'This hash is empty')
     end
   end
 end
