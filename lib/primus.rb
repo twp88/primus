647ce85c3dd44492d6f1b446015a1816dd82f985
Thomas William Page <tom.w.page@gmail.com>
2017-08-25 08:56:06 +0200

    Make rubocop changes

--- lib/primus.rb
+++ lib/primus.rb
@@ -2,12 +2,14 @@
 
 require 'primus/version'
 
+# Takes a hash and returns string of reverse order odd keys
 module Primus
   def call(hash)
     raise 'This hash is empty' if hash.empty?
     array = hash.each_with_index.map { |(key), index| key.upcase if index.odd? }.compact
     array.sort.reverse.join('')
   end
-  # add rubo cop, frozen string, revise sort each with index, each_slice,
-  # slice_each, call this without caiing .new
 end
+
+# add rubo cop, frozen string, revise sort each with index, each_slice,
+# slice_each, call this without caiing .new
